31fe77b10e0093b305ff0a7ca3287764740c37da
Jan Zmeskal <janzmeskal@email.cz>
2020-09-19 19:31:38 +0200

    Start with parametrized test cases

--- calculator_test.go
+++ calculator_test.go
@@ -5,20 +5,43 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a, b, want float64
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
+	var testCases = []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+		{a: -3, b: 5, want: 2},
+		{a: -3, b: -4, want: -7},
+		{a: 0, b: 0, want: 0},
+		{a: 3.14, b: 2, want: 5.14}, // TODO: This fails
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 6
+	got := calculator.Multiply(2, 3)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
